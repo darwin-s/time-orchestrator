7e8ac4e693020eb711c8a87b80d5247e65d67962
darwin-sd <dan.sirbu.sd@gmail.com>
2026-07-01 16:35:32 +0300

    Add flyway migrations

---
+++ user-service/src/main/resources/db/migration-dev/V1.1__add_admin_user.sql
@@ -0,0 +1,6 @@
+INSERT into users (id, profile_url, timezone)
+VALUES (
+        '12ba27f6-6550-433a-b3d0-7203c08deaf9',
+        'https://www.test.com',
+        'Etc/UTC'
+       );
---
+++ user-service/src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id VARCHAR(36) PRIMARY KEY,
+    profile_url VARCHAR(512),
+    timezone VARCHAR(128)
+);
